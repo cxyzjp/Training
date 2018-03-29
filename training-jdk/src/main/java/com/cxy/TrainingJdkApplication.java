a2c0a030d6f299f9765a048c385d82febf4b400b
CZYZJP\张健平 <cxyzjp@foxmail.com>
2018-03-30 00:13:04 +0800

    jdk阅读

---
+++ training-jdk/src/main/java/com/cxy/TrainingJdkApplication.java
@@ -0,0 +1,12 @@
+package com.cxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingJdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingJdkApplication.class, args);
+	}
+}
---
+++ training-jdk/src/main/java/com/cxy/lang/StringDemo.java
@@ -0,0 +1,9 @@
+package com.cxy.lang;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+        String str = "123";
+        System.out.println(str);
+    }
+}
