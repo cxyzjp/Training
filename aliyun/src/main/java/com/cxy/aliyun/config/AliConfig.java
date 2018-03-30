578939e8633ee8af0f8884585828710c22c6f509
Bowen <BowenZhang@fih-foxconn.com.cn>
2018-03-30 17:15:11 +0800

    modify ali

--- aliyun/src/main/java/com/cxy/aliyun/config/AliConfig.java
+++ aliyun/src/main/java/com/cxy/aliyun/config/AliConfig.java
@@ -8,7 +8,7 @@ public class AliConfig {
     public static final String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
     public static final String region = "cn-hangzhou";
     public static final String ossLocation = "oss-cn-hangzhou";
-    public static final String accessKeyId = "LTAID4ynYiSomloM";
+    public static final String accessKeyId = "accessKeyId";
     public static final String accessKeySecret = "accessKeySecret";
     public static final String bucketName = "chuandao01";
     public static final String inputBucketName = "chuandao-in1";
@@ -17,7 +17,7 @@ public class AliConfig {
     public static final String pipelineId = "b51564714aa34c428cbcd0dfcfa3150c";
 
     // 媒体上传用户账号信息
-    public static final String m_accessKeyID = "LTAIlvL4JjetBzFR";
+    public static final String m_accessKeyID = "m_accessKeyID";
     public static final String m_accessKeySecret = "m_accessKeySecret";
     public static final String m_roleArn = "acs:ram::1317421807545751:role/media";
     public static final String m_roleSessionName = "media_user";
